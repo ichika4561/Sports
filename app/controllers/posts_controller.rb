9e0021479633215675dea373184ad89f99a70c48
椎木千翔 <ichika4561@gmail.com>
2019-02-05 20:11:17 +0900

    first

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post =Post.new
+  end
+
+  def edit
+    @post =Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post_url,notice:"投稿「#{post.name}」を更新しました。"
+  end
+  
+  def destroy
+   post = Post.find(params[:id])
+   post.destroy
+   redirect_to posts_url, notice:"投稿#{post.name}を削除しました"
+  end
+
+  def create
+  post = Post.new(post_params)
+  post.save!
+  redirect_to posts_url, notice:"投稿「#{post.name}」を登録しました。"
+  end
+  
+  private
+  def post_params
+   params.require(:post).permit(:name,:descripton)
+  end
+  
+end
