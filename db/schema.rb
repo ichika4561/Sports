d2eadde47f92fe27ca8f3ded4eb9446c8fb0569e
椎木千翔 <ichika4561@gmail.com>
2019-02-06 20:31:49 +0900

    add column name&image

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_02_06_110838) do
     t.datetime "updated_at", null: false
     t.string "uid"
     t.string "provider"
+    t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
