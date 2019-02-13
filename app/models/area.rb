e606d01099efc1e6340c4348a0c1ec4be22e9c75
abgata20000 <abgata@abgata.jp>
2019-02-13 20:54:11 +0900

    Revert "add: relation to model"
    This reverts commit fb8a814a77cf8eb9997b1999e6d830fa1590514e.

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,3 @@
 class Area < ApplicationRecord
-    has_many :posts
     validates :area, presence: true
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,3 @@
 class Genre < ApplicationRecord
-    has_many :posts
     validates :genre, presence: true
 end
