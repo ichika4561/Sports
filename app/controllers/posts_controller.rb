4b93775be32d170cbab866ce1a28a9bdaece0cae
さき大山 <soraday246@gmail.com>
2019-02-22 00:20:17 +0900

    fix detail

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  # _search.html.slimにて＠qを返す。
   def index
     @q = Post.where("started_at > ?", Time.zone.now).order(started_at: :asc).ransack(params[:q])
     @posts = @q.result(distinct: true).page(params[:page])
