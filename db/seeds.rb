c116c4cc1cb36ddee514ec0798e94dceace6dbab
さき大山 <soraday246@gmail.com>
2019-03-07 13:42:08 +0900

    genre

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,34 +2,47 @@
 
 #都道府県のファイルを読み込む。
 require 'csv'
-require 'zip'
+# require 'zip'
 
-DLURL           = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
-SAVEDIR         = "db/"
-CSVROW_AREANAME = 6
+# DLURL           = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
+# SAVEDIR         = "db/"
+# CSVROW_AREANAME = 6
 
-savePath = ""
+# savePath = ""
 
-# 住所CSVダウンロード、解答し保存
-open(URI.escape(DLURL)) do |file|
-  ::Zip::File.open_buffer(file.read) do |zf|
-    zf.each do |entry|
-      savePath = SAVEDIR + entry.name
-      zf.extract(entry, savePath) { true }
-    end
-  end
+# # 住所CSVダウンロード、解答し保存
+# open(URI.escape(DLURL)) do |file|
+#   ::Zip::File.open_buffer(file.read) do |zf|
+#     zf.each do |entry|
+#       savePath = SAVEDIR + entry.name
+#       zf.extract(entry, savePath) { true }
+#     end
+#   end
+# end
+
+# # 住所CSVを読み込みDBに保存
+# CSV.foreach(savePath, encoding: "Shift_JIS:UTF-8") do |row|
+#   areaName = row[CSVROW_AREANAME]
+#   area = Area.find_or_create_by(:name => areaName)
+# end
+
+# File.unlink savePath
+
+# 競技用
+#csvファイルを扱うためのgemを読み込む
+
+#foreachは、ファイル（genre.csv）の各行を引数として、ブロック(do~endまでを範囲とする『引数のかたまり』)を繰り返し実行する
+#rowには、読み込まれた行が代入される
+CSV.foreach("db/genre.csv") do |row|
+  Genre = Genre.create(
+    id:   row['id'],
+    name: row['name'],
+  )
 end
 
-# 住所CSVを読み込みDBに保存
-CSV.foreach(savePath, encoding: "Shift_JIS:UTF-8") do |row|
-  areaName = row[CSVROW_AREANAME]
-  area = Area.find_or_create_by(:name => areaName)
-end
-
-File.unlink savePath
 
 # 順番を指定して読み込みたいファイルを指定
-seed_files = ["user.rb", "genre.rb"]
+seed_files = ["user.rb"]
 
 # seedsフォルダを検索して実行するファイルを追加
 Dir.glob(File.join(Rails.root, "db", "seeds", "*.rb")) do |file|
